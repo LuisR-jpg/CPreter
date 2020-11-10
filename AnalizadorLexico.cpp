ba5247eb2ddde08b41a7dea7a5eb35ddfb45406b
Lalito Robles <laloro08@hotmail.com>
2020-11-10 09:56:31 -0600

    Espacios y ligeras modificaciones

--- AnalizadorLexico.cpp
+++ AnalizadorLexico.cpp
@@ -22,55 +22,54 @@ class AnalizadorLexicoV2
             string aux = "";
             for(int i = 0; i < code.size(); i++)
             {
-                //cout << i << " " << code[i] << endl;
-                //cout << code[code.size()-1] << endl;
                 if(code[i] == ' ')
                 {
-                    //esp++;
-                    if(++esp == 4) token.push_back(make_pair("\\t",50)), esp = 0;
+                    if(++esp == 4)
+		      token.push_back(make_pair("\\t",50)), esp = 0;
                     continue;
                 }
-                if(isalpha(code[i]))//else if(isalpha(code[i]))
+                if(isalpha(code[i]))
                 {
-                    //while(code[i]!=' '&&code[i]!='\n'&&code[i]!='\t')
-                    while(isalnum(code[i])||code[i]=='_')
-                        aux+=code[i], i++;//cout << code[i] << endl,
-                    if(aux=="db") token.push_back(make_pair("db",10));
-                    else if(aux=="di") token.push_back(make_pair("di",10));
-                    else if(aux=="dd") token.push_back(make_pair("dd",10));
-                    else if(aux=="ds") token.push_back(make_pair("ds",10));
-                    else if(aux!="") token.push_back(make_pair(aux,00));
+                    while(isalnum(code[i]) || code[i] == '_')
+                        aux += code[i], i++;
+                    if(aux == "db") token.push_back(make_pair("db", 10));
+                    else if(aux == "di") token.push_back(make_pair("di", 10));
+                    else if(aux == "dd") token.push_back(make_pair("dd", 10));
+                    else if(aux == "ds") token.push_back(make_pair("ds", 10));
+                    else if(aux != "") token.push_back(make_pair(aux, 0));
                     aux = "";
                 }
                 if(isdigit(code[i]))//En estos, checar casos como 15*9. Eliminar else? else if(isdigit(code[i]))
                 {
-                    while(isdigit(code[i])||code[i]=='.')
-                        aux+=code[i], i++;//cout << code[i] << endl,
-                    token.push_back(make_pair(aux,60));
+                    while(isdigit(code[i]) || code[i]=='.')
+                        aux += code[i], i++;
+                    token.push_back(make_pair(aux, 60));
                     aux = "";
                 }
-                if(code[i]=='"')//else if(code[i]=='"')
+                if(code[i] == '"')
                 {
+		  /*
                     i++;
-                    while(code[i]!='"')
-                        aux+=code[i], i++;//cout << code[i] << endl,
-                    token.push_back(make_pair(aux,70));
-                    i++;
-                    aux = "";
+                    while(code[i] != '"')
+                        aux += code[i], i++;
+		  */
+		    while(code[++i] != '"')
+			aux += code[i];
+                    token.push_back(make_pair(aux, 70));
+                    i++, aux = "";
                 }
-                if(ispunct(code[i]))//else if(ispunct(code[i]))<
+                if(ispunct(code[i]))
                 {
-                    //while(ispunct(code[i]))
-                        //aux+=code[i], i++;//cout << code[i] << endl,
-                    if(code[i+1]=='='||code[i+1]=='|'||code[i+1]=='&'||code[i+1]=='>') aux += code[i], aux += code[i+1], i++;
+                    if(code[i+1] == '=' || code[i+1] == '|' || code[i+1] == '&' || code[i+1] == '>')
+		      aux += code[i], aux += code[i+1], i++;
                     else aux += code[i];
-                    token.push_back(make_pair(aux,30));
+                    token.push_back(make_pair(aux, 30));
                     aux = "";
                 }
-                if(code[i]=='\n') token.push_back(make_pair("\\n",50));
+                if(code[i] == '\n') token.push_back(make_pair("\\n",50));
                 esp = 0;
-                //aux = "";
             }
+
         }
 };
 
