ef0cb24abddd54b1c2f665f58fb0bd351f5e31cd
Lalito Robles <laloro08@hotmail.com>
2020-11-13 14:27:39 -0600

    Doble punto controlado

--- AnalizadorLexico.cpp
+++ AnalizadorLexico.cpp
@@ -29,7 +29,7 @@ class AnalizadorLexicoV2
         }
         void insert(string a)
         {
-            if(a == "true" || a == "false") insert(a, 52);// 10 = palabra reservada (valor bool)
+            if(a == "true" || a == "false") insert(a, 52);// 52 = palabra reservada (valor bool)
             else if(type.find(a) != type.end()) insert(a, 10);// 10 = palabra reservada (tipo de dato)
             else if(instruction.find(a) != instruction.end()) insert(a, 20);// 20 = palabra reservada (instruccion)
             else insert(a, 0);//00 = identificador
@@ -44,10 +44,9 @@ class AnalizadorLexicoV2
             string aux = "";
             for(int i = 0; i < code.size(); i++)
             {
-                if(code[i] == ' ')
+                if(code[i] == ' ' || code[i] == '\t')
                 {
-                    if(++esp == 4)
-                    insert("\\t", 60), esp = 0;//60 = \t (identación)
+                    if(++esp == 4 || code[i] == '\t') insert("\\t", 60), esp = 0;//60 = \t (identación)
                     continue;
                 }
                 if(isalpha(code[i]))
@@ -60,13 +59,19 @@ class AnalizadorLexicoV2
                 if(isdigit(code[i]))
                 {
                     bool intodouble = true;
-                    while(isdigit(code[i]) || code[i]=='.')
+                    int pun = 0;
+                    while(isdigit(code[i]) || code[i]=='.')//Caso 10.5.2.1 = token inválido///////////////////
                     {
                         aux += code[i];
-                        if(code[i] == '.') intodouble = false;
+                        if(code[i] == '.') intodouble = false, pun++;
                         i++;
                     }
-                    if(intodouble) insert(aux, 50);//50 = valor entero
+                    if(intodouble && pun == 1) insert(aux, 50);//50 = valor entero
+                    else if(pun>1)
+                    {
+                        cout << "Error" << endl;
+                        return;
+                    }
                     else insert(aux, 51);//51 = valor decimal
                     aux = "";
                 }
@@ -88,7 +93,7 @@ class AnalizadorLexicoV2
                     if(code[i+1] == '=' || code[i+1] == '|' || code[i+1] == '&' || code[i+1] == '>')
                         aux += code[i], aux += code[i+1], i++;
                     else aux += code[i];
-                    if(aux == "->" || aux == "," || aux == ".") insert(aux,30);//30 = Símbolos especiales
+                    if(aux == "->" || aux == "," || aux == "=") insert(aux,30);//30 = Símbolos especiales
                     else if(aux == "(" || aux == ")") insert(aux,31);//31 = Símbolos de jerarquía
                     else if(aux == "+" || aux == "-" || aux == "*" || aux == "/") insert(aux,32);//32 = Símbolos de aritmética
                     else if(aux == "<" || aux == ">" || aux[1] == '=') insert(aux,33);//33 = Símbolos relacionales
@@ -100,7 +105,9 @@ class AnalizadorLexicoV2
             }
         }
 };
-string read(){
+
+string read()
+{
     ifstream file;
     string a, b;
     file.open("in.cpr", ios::in);
@@ -108,14 +115,22 @@ string read(){
     while(!file.eof()) getline(file, a), a += "\n", b += a;
     return b;
 }
-    
+
 int main()
 {
     string s = read();
     cout << s << endl;
     AnalizadorLexicoV2 a(s);
     //AnalizadorLexicoV2 a("    dd variable1=35.4\nds variable2\nvariable2=\"hola\"\nsi variable1<=variable2\n'c' 30 true false && / (");//("    db\ndi")
+    cout << endl;
     for(int i = 0; i < a.token.size(); i++)
         cout << a.token[i].first << " " << a.token[i].second << endl;
     return 0;
 }
+/*
+Archivos separados
+Archivo por clase, para cada instrucción
+1. Hacer clases
+2. Analizador sintáctico (token por token)
+3. Vector con tokens?
+*/
