02078fd29da66022cd5cffd0c9a854ecf5b9411b
Lalito Robles <laloro08@hotmail.com>
2020-11-22 08:06:57 -0600

    22 de Noviembre

--- Branches/ExpressionEvaluator.h
+++ Branches/ExpressionEvaluator.h
@@ -5,14 +5,39 @@ class Evaluator{
   public: 
     class Operator{
       public: 
-        char symbol;
-        Operator(char symbol): symbol(symbol){}
+        string symbol;
+        Operator(string symbol): symbol(symbol){}
         ~Operator(){}
     };
     vector<pair<string, int>> expression;
     Evaluator(vector<pair<string, int>> expression): expression(expression){}
-    double evaluate(){
-      for(auto i: expression){
-        if(i.second == 10)
+    double evaluate()
+    {
+      for(auto i: expression)
+      {
+        if(i.second == 10) out.push(i);
+	else if(sta.size() && sta.front() != "(")
+	{
+	  while(sta.front().priority() >= i.priority())
+	    out.push(sta.pop());
+	  sta.push(i);
+	}
+	else if(i.first == "(") sta.push(i);
+	else if(i.first == ")"){
+	  while(sta.front().first != "(") out.push(sta.pop());
+	  sta.pop();
+	  //Lanza error si no hay parentestis (
+	}
+      }
+      while(sta.size())
+      {
+	if(sta.front() == "(" || sta.front == ")") cout << "F"; //Need to be handled
+	out.push(sta.pop());
+      }
+      while(out.size()) cout << out.pop().first << " ";
+    }
+};
+
+
 
 
--- Branches/test.cpp
+++ Branches/test.cpp
@@ -1,2 +1,1 @@
 #include "ExpressionEvaluator.h"
-:
